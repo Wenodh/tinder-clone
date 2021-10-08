f2da9aca244324d8e69bc0b510d583180d1867e0
wenodh <vinodkumara3@gmail.com>
2021-10-08 21:43:00 +0530

    bug fix

--- capstone/app/config/db.config.js
+++ capstone/app/config/db.config.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 module.exports = {
-    url: `mongodb+srv://m001-student:m001-mongodb-basics@sandbox.wrvbj.mongodb.net/capstone?retryWrites=true&w=majority`,
+    url: `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@sandbox.wrvbj.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`,
 };
 //     HOST: 'localhost',
 //     PORT: 27017,
